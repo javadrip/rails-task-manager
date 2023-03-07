ceba02ae433c1acc741a3719351a63722494e8bc
Vinleon Ang <javadripdev@gmail.com>
2023-03-07 16:26:50 +0800

    Task add update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
-  get "tasks/:id/edit", to: "tasks#edit"
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update'
+  delete "tasks/:id", to: "tasks#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
