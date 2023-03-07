2edffa1ea937a18ab3ec110c1785b4b26b697f2a
Vinleon Ang <javadripdev@gmail.com>
2023-03-07 16:39:55 +0800

    Task refactor using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
-  patch 'tasks/:id', to: 'tasks#update'
-  delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
+  ### Line 3 sums up line 5 to 11.
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete "tasks/:id", to: "tasks#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
